6aa9790c4be015ce3b67b6081988a37de0d5c968
easesstone <576395455@qq.com>
2017-06-24 11:12:28 +0800

    to add file to test hdfs

--- demoldl/src/test/java/FileSystemCat.java
+++ demoldl/src/test/java/FileSystemCat.java
@@ -1,8 +1,11 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 
 public class FileSystemCat {
@@ -11,5 +14,10 @@ public class FileSystemCat {
        String uri = "hdfs://master:9000/test/hadoop-root-namenode-master.log";
         Configuration conf = new Configuration();
         FileSystem fs = FileSystem.get(URI.create(uri), conf);
+        InputStream in = null;
+        in = fs.open(new Path(uri));
+        IOUtils.copyBytes(in, System.out, 4096, false);
+        IOUtils.closeStream(in);
+
     }
 }
